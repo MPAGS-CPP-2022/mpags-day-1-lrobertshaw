5b0846a0d3b57933920374ddd046de850ab9d1b2
wq22321 <liam.robertshaw@bristol.ac.uk>
2022-10-19 13:35:13 +0000

    exercise8

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -6,15 +6,27 @@ using namespace std;
 
 int main(int argc, char* argv[]){
     
-    vector<int> vect{};
-    vector<int> vect2 = {1,2,3,4};
-    vector<string> vect_string = {"Liam", "Nathan", "Shahzad"};
-    
-    for(auto i = 0; i < vect_string.size(); i++){
-        cout << vect_string[i] << endl;
+    const vector<string> cmdLineArgs = {argv, argv+argc};
+
+    string inputFile{};
+    string outputFile{};
+
+    for(int i = 0; i < cmdLineArgs.size(); i++){
+        if(cmdLineArgs[i] == "--help" || cmdLineArgs[i] == "-h"){
+            cout << "This program is mpags-cipher" << endl;
+        }
+        if(cmdLineArgs[i] == "--version"){
+            cout << "Version 0.1.0" << endl;
+        }
+        if(cmdLineArgs[i] == "--input" || cmdLineArgs[i] == "-i"){
+            inputFile = cmdLineArgs[i+1];
+            cout << inputFile << endl;
+        }
+        if(cmdLineArgs[i] == "--output" || cmdLineArgs[i] == "-o"){
+            outputFile = cmdLineArgs[i+1];
+            cout << outputFile << endl;
+        }
     }
-
-    //cout << vect_string << endl;
-
+    
     return 0;
 }
